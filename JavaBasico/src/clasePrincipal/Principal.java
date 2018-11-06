b804ec9f076a699e5125f5aa4315119eab957ed2
mcvasqu <mcvasqu@bancolombia.com.co>
2018-11-06 10:14:33 -0500

    Primer Commit

---
+++ JavaBasico/src/clasePrincipal/Principal.java
@@ -0,0 +1,84 @@
+package clasePrincipal;
+
+import java.io.Console;
+import java.util.logging.ConsoleHandler;
+
+import miOtraClase.MiOtraClase;
+
+public class Principal {
+
+	static int numero1 = 1;
+	static int numero2 = 2;
+	static int resultado = 0;
+	
+	public static void main(String[] args) {
+	
+//		//Tipos de variables basicos
+//		String cadenaDePalabras = "Ejemplo";
+//		String cadenaDeNumeros= "121312";
+//		String cadenaDePalabrasYNumeros = "ejemplo123";
+//		
+//		int numeros = 1;
+//		int numerosMasGrandes = 12312;
+//		
+//		double numeroDecimal = 1.2;
+//		
+//		
+//		char caracterDeTexto = 'a';
+//		char caracterNumerico = '1';
+//		
+//		
+//		
+//		
+//		
+//		//Sentencias logicas
+//		if("hola" == "hola") 
+//		{
+//			System.out.println("hola SI es igual a la otra palabra");
+//		}
+//		else
+//		{
+//			System.out.println("hola NO es igual a la otra palabra");
+//		}
+//		
+//		
+//		
+//		switch ("hola") {
+//		case "hola":
+//			System.out.println("encontramos la palabra hola asi que realizamos las acciones de este caso");
+//			break;
+//			
+//			
+//		case "adios":
+//			System.out.println("encontramos la palabra adios asi que realizamos las acciones de este caso");
+//			break;
+//			
+//			
+//		case "hasta luego":
+//			System.out.println("encontramos la palabra hasta luego asi que realizamos las acciones de este caso");
+//			break;
+//			
+//			
+//		default:
+//			System.out.println("no encontramos ningun caso que coincida con las palabra que buscabas asi que realizamos las "
+//					+ "acciones por default");
+//			break;
+//		}
+//			
+//		
+//		
+//		//ciclo mas utilizado
+//		//Un ciclo es unas sentencias que se repiten varias veces 
+//		for (int numeroQueVaria = 0; numeroQueVaria <= 10; numeroQueVaria= numeroQueVaria+1) {
+//			
+//			System.out.println("esta es la vuelta: "+ numeroQueVaria +" y el numero que varia va en :"+numeroQueVaria);
+//			
+//		}
+//		
+//		
+
+	}
+	
+	
+    
+}
---
+++ JavaBasico/src/miOtraClase/MiOtraClase.java
@@ -0,0 +1,33 @@
+package miOtraClase;
+
+public class MiOtraClase {
+
+	
+	//Constructor
+	//Es crear un metodo con el nombre de la clase
+	//Nos sirve como un punto de acceso hacia esta clase
+	public MiOtraClase() {
+		
+	}
+	
+	private String palabraDeMiOtraClase = "agua";
+	
+	
+	
+	
+	//sirve para llenar la variable
+	public void setPalabraDeMiOtraClase(String nuevoValor) {
+		palabraDeMiOtraClase = nuevoValor;
+	}
+	
+	
+	//sirve para sacar el valor que tiene la variable
+	public String getPalabraDeMiOtraClase() {
+		return palabraDeMiOtraClase;
+	}
+	
+	
+
+	
+	
+}
